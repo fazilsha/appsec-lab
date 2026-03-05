837fd9a1022af27672a161eeec9ac79c21fa2043
Fazil sha <fazilsha@Fazils-MacBook-Air.local>
2026-03-05 05:37:41 +0530

    Add vulnerable code for AppSec testing

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const csrf = require('csurf');
+const { exec } = require('child_process');
 
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
+const API_KEY = "123456-SECRET-KEY-ADMIN"; // hardcoded secret
 
-// Enable CSRF protection
-app.use(csrf({ cookie: true }));
+app.get('/user', (req, res) => {
+    const user = req.query.user;
 
-app.get('/', (req, res) => {
-    res.send("Secure App Running");
+    const query = "SELECT * FROM users WHERE username = '" + user + "'";
+    console.log(query);
+
+    res.send("User lookup executed");
+});
+
+app.get('/ping', (req, res) => {
+    const host = req.query.host;
+
+    exec("ping -c 1 " + host, (err, stdout) => {
+        res.send(stdout);
+    });
 });
 
 app.listen(3030, () => {
-    console.log("Secure app running on port 3030");
+    console.log("Secure App Running");
 });
