e280ef6f74b93c5fdba9f6f9e62d614c2c2ecad2
Fazil sha <fazilsha@Fazils-MacBook-Air.local>
2026-03-10 19:26:26 +0530

    Trigger Jenkins build - code change

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
 const express = require('express');
-//const { exec } = require('child_process');
-
 const app = express();
 
-//const API_KEY = "123456-SECRET-KEY-ADM
-/**
-app.get('/user', (req, res) => {
-    const user = req.query.user;
-
-    const query = "SELECT * FROM users WHERE username = '" + user + "'";
-    console.log(query);
-
-    res.send("User lookup executed");
-});
-
-app.get('/ping', (req, res) => {
-    const host = req.query.host;
-
-    exec("ping -c 1 " + host, (err, stdout) => {
-        res.send(stdout);
-    });
-});
-
-app.listen(3030, () => {
-    console.log("Secure App Running");
-});// webhook test
-**/
