a89bfc6484935d618cc5d91ea8ba64bda4d59345
Fazil sha <fazilsha@Fazils-MacBook-Air.local>
2026-03-11 00:38:22 +0530

    Add vulnerable JS app

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    eval(req.query.code);   // intentional vulnerability
+    res.send("Hello AppSec");
+});
+
+app.listen(3030);
